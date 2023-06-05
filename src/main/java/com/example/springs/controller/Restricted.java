388f002cc83e720acc8cdf182bf6138c44e1f2b0
Santosh Timilisina <santos.timil@gmail.com>
2023-06-05 23:45:45 +0545

    hide-class-or-method-from-swagger

--- src/main/java/com/example/springs/controller/Restricted.java
+++ src/main/java/com/example/springs/controller/Restricted.java
@@ -1,14 +1,17 @@
 package com.example.springs.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/restricted")
+@Hidden  //we can also apply in swagger
 public class Restricted {
 
     @GetMapping
+    @Hidden // this hides this method in swagger
     public String restrictedContent() {
         return "This is restricted";
     }
